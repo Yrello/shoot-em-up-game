880f592f6a9302785d589c8f8b12e367b42773b1
Yrello <52930426+Yrello@users.noreply.github.com>
2021-03-01 16:41:34 +0400

    Add files via upload

--- Hero.py
+++ Hero.py
@@ -1,6 +1,10 @@
-class Hero:
+import pygame as pg
+import random as rnd
 
-    def __init__(self, x, y, hp, v, a, sp, damage):
+class Hero(pg.sprite.Sprite):
+
+    def __init__(self, x, y, hp, v, a, sp, damage, image, *group):
+        super(Hero, self).__init__(*group)
         self.x = x
         self.y = y
         self.hp = hp
@@ -8,49 +12,158 @@ class Hero:
         self.a = a
         self.sp = sp  # mb it will be shield points smts
         self.damage = damage
+        self.ss = pg.mixer.Sound('audio/laser.wav')
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.group = group
 
-    def move(self, dir):
+    def debug(self):
+        print(self.v)
+
+    def get_dir(self, dir):
         self.dir = dir
         if self.dir == 'up':
-            self.v = (0, 1)
+            self.v[1] -= 1
         if self.dir == 'down':
-            self.v = (0, -1)
+            self.v[1] += 1
         if self.dir == 'left':
-            self.v = (-1, 0)
+            self.v[0] -= 1
         if self.dir == 'right':
-            self.v = (1, 0)
+            self.v[0] += 1
         if self.dir == 'none':
-            self.v = (0, 0)
+            self.v = [0, 0]
 
-        self.x, self.y = self.sp * self.v[0], self.sp * self.v[1]
+    def set_dir(self, a, n):
+        self.v[a] = n
+
+    def move(self):
+        if pg.sprite.spritecollideany(self, ):
+            print('AAA')
+
+        if self.a[0] != 0 or self.a[1] != 0:
+            self.a[0] /= 1.2 ** 0.5
+            self.a[1] /= 1.2 ** 0.5
+        if abs(self.a[0]) < 5:
+            self.a[0] += self.v[0] / 5
+        if abs(self.a[1]) < 1.5:
+            self.a[1] += self.v[1] / 5
+
+        self.x, self.y = self.x + self.sp * self.a[0], self.y + self.sp * self.a[1]
+        self.rect.x = self.x
+        self.rect.y = self.y
+        '''if abs(self.v[0]) == abs(self.v[1]):
+            self.x /= 2**0.5
+            self.y /= 2**0.5'''
 
     def get_coord(self):
-        return self.x, self.y
+        return (self.x, self.y)
 
-    def shoot(self, x, y, dir, dmg, sp, angsp=0):
-        b = Bullet(x, y, dir, dmg, sp, angsp)
-        b.shoot()  # dir 1/-1, damage, speed
+    def shoot(self, image, x, y, dir, dmg, sp, angsp, group):
+        b = Bullet(image, x, y, dir, dmg, sp, angsp, group)
+        self.ss.play()
+        # b.shoot()  # dir 1/-1, damage, speed
+        return b
+
+    def get_info(self):
+        return [self.x, self.y, self.hp, self.damage]
 
 
-class Bullet():
-    def __init__(self, x, y, dir, dmg, sp, angsp=0):
+class Bullet(pg.sprite.Sprite):
+    def __init__(self, image, x, y, dir, dmg, sp, angsp, *groups):
+        super().__init__(*groups)
         self.dir = dir
         self.dmg = dmg
         self.sp = sp
         self.angsp = angsp
-        self.x = x
+        self.x = x + 35
         self.y = y
+        self.image = image
+        self.rect = self.image.get_rect()
 
     def shoot(self):
         self.y += self.sp * self.dir
         self.x += self.angsp
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        # print(self.x,self.y)
+        # print('boom')
 
     def get_coord(self):
         return self.x, self.y
 
+    def get_sprite(self):
+        return self.sprite
 
-class Enemy(Hero):
-    def __init__(self, x, y, hp, v, a, sp, damage):
-        super().__init__(x, y, hp, v, a, sp, damage)
+
+class Enemy(pg.sprite.Sprite):
+    def __init__(self, image, x, y, hp, v, a, sp, damage, sprite, *groups):
+        super().__init__(*groups)
+        self.x = x
+        self.y = y
+        self.hp = hp
+        self.v = v
+        self.a = a
+        self.delta = rnd.randint(-30,30)/15
+        #self.delta = self.delta / 2 - self.delta
+        self.sp = sp + self.delta
+        self.damage = damage
+        self.image = image
+        self.sprite = sprite
+        self.rect = self.image.get_rect()
+        print(self.delta)
+    def get_coord(self):
+        return self.x, self.y
+
     def move(self, plx, ply):
-        pass
+        deltax = rnd.randint(1, 10) / 10
+        deltax = deltax / 2 - deltax
+        if self.x < plx + deltax:
+            self.x += self.sp
+        if self.x > plx + deltax:
+            self.x -= self.sp
+        if self.y < 50 + self.delta * 30:
+            self.y += self.sp
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+
+
+
+class EnemyAI:
+    def __init__(self):
+        self.lst = []
+        self.dif = 0
+        self.std_dif = 11
+        self.enemy_aggression = 0.5
+
+    def get_enemy_info(self, a):
+        return a.get_coord()
+
+    def get_screen_info(self, game_time, enemy_list, player_info, bullet_list):
+        self.time = game_time
+        #self.lst = enemy_list
+        self.player_info = player_info
+        self.bullets = bullet_list
+
+    def get_enemy_list(self):
+        return self.lst
+
+    def difficult_level_calc(self):
+        self.dif = len(self.lst) + len(self.bullets) / 5 + 1 / (self.player_info[2]) + 1 / (self.player_info[3]) + \
+                   self.time / 100 + self.enemy_aggression ** 2  # 7 + 12/5 + 1/2 + 1/5 + 1
+
+    def difficult_level_change(self,img, sprite, group):
+        if self.dif < self.std_dif:
+            if rnd.randint(0, 2) == 0:
+                e = Enemy(img, 30 + rnd.randint(-20, 60), -30, 3, [0, 0], [0, 0], 2, 2, sprite, group)
+                self.lst.append(e)
+            else:
+                self.enemy_aggression += 0.15
+        if self.dif > self.std_dif:
+            self.std_dif -= 0.07
+
+    def act(self, plcoord):
+        #print(self.lst)
+        for i in self.lst:
+            i.move(plcoord[0],plcoord[1])
--- noise.py
+++ noise.py
@@ -9,6 +9,30 @@ from Hero import *
 import os
 import sys
 
+horizontal_borders = pg.sprite.Group()
+vertical_borders = pg.sprite.Group()
+
+class Border(pg.sprite.Sprite):
+    # строго вертикальный или строго горизонтальный отрезок
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:  # вертикальная стенка
+            self.add(vertical_borders)
+            self.image = pg.Surface([1, y2 - y1])
+            self.rect = pg.Rect(x1, y1, 1, y2 - y1)
+        else:  # горизонтальная стенка
+            self.add(horizontal_borders)
+            self.image = pg.Surface([x2 - x1, 1])
+            self.rect = pg.Rect(x1, y1, x2 - x1, 1)
+
+
+width = 500
+height = 700
+'''Border(5, 5, width - 5, 5)
+Border(5, height - 5, width - 5, height - 5)
+Border(5, 5, 5, height - 5)
+Border(width - 5, 5, width - 5, height - 5)'''
+
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
     try:
@@ -22,7 +46,6 @@ def load_image(name, color_key=None):
             color_key = image.get_at((0, 0))
         image.set_colorkey(color_key)
     return image
-
 
 
 pg.init()
@@ -39,22 +62,40 @@ map = []
 
 all_sprites = pg.sprite.Group()
 enemies = pg.sprite.Group()
-bullets = pg.sprite.Group()
-powerups = pg.sprite.Group()
 
+powerups = pg.sprite.Group()
+bullets = pg.sprite.Group()
 background = load_image("background2.png")
 background_rect = background.get_rect()
 
 hero_image = pg.transform.scale(load_image("ship.png"), (
-    74, 96)) # размер изображения: 37*48
+    74, 96))  # размер изображения: 37*48
 mini_image_ship = pg.transform.scale(load_image("mini_image_ship.png"), (
-    28, 40)) # размер изображения: 14*20
-laser_img = pg.transform.scale(load_image("laser.png"), (
-    8, 18)) # размер изображения: 4*9
+    28, 40))  # размер изображения: 14*20
+laser_img = pg.transform.scale(load_image("laser.png"), (8, 18))  # размер изображения: 4*9
+enemy_img = pg.transform.scale(load_image("test_enemy.png"), (28*2, 18*2))
+
 sprite_group, hero_group = pg.sprite.Group(), pg.sprite.Sprite(all_sprites)
 hero_group.image = hero_image
+hero_group.rect = hero_group.image.get_rect()
+hero_group.rect.x = -100
+hero_group.rect.y = -100
 
 
+enemy_sprite = pg.sprite.Sprite()
+enemy_sprite.image = enemy_img
+enemy_sprite.rect = enemy_sprite.image.get_rect()
+enemy_sprite.rect.x = -10
+enemy_sprite.rect.y = -70
+all_sprites.add(enemy_sprite)
+
+
+laser_sprite = pg.sprite.Sprite()
+laser_sprite.image = laser_img
+laser_sprite.rect = laser_sprite.image.get_rect()
+laser_sprite.rect.x = -50
+laser_sprite.rect.y = -50
+all_sprites.add(laser_sprite)
 
 '''for i in range(16):
     s = []
@@ -74,7 +115,7 @@ tic = time()
 a = upscale(a, 64, 64)
 a = np.array(a,"float")
 a = gaussian_filter(a, sigma=20)
-#map2 = blur(map2, 20)
+#map2 = blur(map2, 20)1
 toc = time()
 print(toc - tic)
 
@@ -90,56 +131,83 @@ a = generate(4,4,128,50,1)
 map = add(map, a, 0.5)
 t2 = time()
 print(t2-t1)'''
-#map2 = ship()
-#map = []
+# map2 = ship()
+# map = []
 
-map = generate(32,32,4,3)
+map = generate(32, 32, 4, 3)
 
 for i in range(len(map)):
     for j in range(len(map[0])):
         map[i][j] **= 4
-#for i in map2:
+# for i in map2:
 #    map.append(i[::-1]+i)
 
-#pprint(map)
+# pprint(map)
 
-#map = upscale(map,16,16)
-clr = (200,200,200)
-hero = Hero(220, 500, 5, 2, 0, 5, 10)
-hero_group.rect.x = hero.get_coord()[0]
-hero_group.rect.y = hero.get_coord()[1]
-
+# map = upscale(map,16,16)
+ai = EnemyAI()
+clr = (200, 200, 200)
+hero = Hero(220, 500, 5, [0, 0], [0, 0], 5, 10, hero_image, all_sprites)
+#hero_group.rect.x = hero.get_coord()[0]
+#hero_group.rect.y = hero.get_coord()[1]
+bulls = []
 while running:
+    keys = pg.key.get_pressed()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
                 running = False
-            elif event.key == pg.K_p:
+            if event.key == pg.K_p:
                 pause = True
-            elif event.key == pg.K_UP or event.key == pg.K_w:
-                hero.move("up")
-            elif event.key == pg.K_DOWN or event.key == pg.K_s:
-                hero.move("down")
-            elif event.key == pg.K_LEFT or event.key == pg.K_a:
-                hero.move("left")
-            elif event.key == pg.K_RIGHT or event.key == pg.K_d:
-                hero.move("right")
-            elif event.key == pg.K_SPACE:
-                hero.shoot(hero.get_coord()[0],hero.get_coord()[1],-1,2,2)
+            if event.key == pg.K_UP or event.key == pg.K_w:
+                hero.get_dir("up")
+            if event.key == pg.K_DOWN or event.key == pg.K_s:
+                hero.get_dir("down")
+            if event.key == pg.K_LEFT or event.key == pg.K_a:
+                hero.get_dir("left")
+            if event.key == pg.K_RIGHT or event.key == pg.K_d:
+                hero.get_dir("right")
+
+            if event.key == pg.K_SPACE:
+                b = hero.shoot(laser_img, hero.get_coord()[0], hero.get_coord()[1], -1, 2, 10, 0, all_sprites)
+
+                all_sprites.add(laser_sprite)
+                bulls.append(b)
+
+        else:
+            hero.get_dir('none')
+    ai.get_screen_info(1,[],hero.get_info(),bulls)
+    ai.difficult_level_calc()
+    ai.difficult_level_change(enemy_img, enemy_sprite, all_sprites)
+    ai.act(hero.get_coord())
+    #print(ai.get_enemy_list)
+    # hero.debug()
+    # print(len(bulls))
+    hero.move()
+    for i in bulls:
+        i.shoot()
+        c = i.get_coord()
+        if c[1] < -20:
+            bulls.remove(i)
+    #hero_group.rect.x = hero.get_coord()[0]
+    #ёhero_group.rect.y = hero.get_coord()[1]
+
+    # laser_sprite.rect.x =
 
     screen.fill(pg.Color("black"))
     screen.blit(background, background_rect)
     # hero_group.rect
-    for i in range(len(map)):
-        for j in range(len(map[0])):
-            #screen.set_at((j, i), (int(map[i][j] * 255), int(map[i][j] * 255), int(map[i][j] * 255)))
-            pass
-            #if map[i][j] == 1:
-            #    screen.set_at((j, i), clr)
+    # for i in range(len(map)):
+    # for j in range(len(map[0])):
+    # screen.set_at((j, i), (int(map[i][j] * 255), int(map[i][j] * 255), int(map[i][j] * 255)))
+    # pass
+    # if map[i][j] == 1:
+    #    screen.set_at((j, i), clr)
     all_sprites.draw(screen)
+    bullets.draw(screen)
     clock.tick(FPS)
-    #pg.display.flip()
+    # pg.display.flip()
     pg.display.update()
 pg.quit()
